8c1e42fc28283f381e635bcc568ace880eebee12
EslamKamel89 <elsamkamelforex@gmail.com>
2026-06-08 00:58:33 +0300

    chore(models): scaffold domain models and factories for work order system

---
+++ database/factories/WorkOrderAttachmentFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\WorkOrderAttachment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<WorkOrderAttachment>
+ */
+class WorkOrderAttachmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/WorkOrderCommentFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\WorkOrderComment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<WorkOrderComment>
+ */
+class WorkOrderCommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
